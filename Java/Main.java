5bde82d322dda04faf4aaa869b4d0183bb03668b
PiperPimientos <feliperestrepomeneses@gmail.com>
2021-08-22 13:13:41 -0500

    Agregamos un archivo index.html para poder empezar a hacer el metodo constructor en JavaScript, pero primero tuvimos que hacer el metodo constructor en Java, donde modificamos los archivos Car.java y Account.java para agregar los metodos constructores y luego el archivo Main, incluir dentro del objeto car y car2 dicho metodo constructor

--- Java/Main.java
+++ Java/Main.java
@@ -1,15 +1,11 @@
 class main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
-        Car car = new Car();
-        car.licence = "AMQ123";
-        car.driver = "Andres Herrera";
+        Car car = new Car("AMQ123", new Account("Andres Herrera", "AND123"));
         car.passengers = 4;
         car.printDataCar();
 
-        Car car2 = new Car();
-        car2.license =  "QWE576";
-        car2.driver = "Andrea Herrera";
+        Car car2 = new Car("QWE576", new Account("Andrea Herrera", "ANDA876"));
         car2.passengers = 3;
         car2.printDataCar();
 
