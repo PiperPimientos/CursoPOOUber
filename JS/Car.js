4597654cdde893477b68fc0e1c3bbcdca7c64979
PiperPimientos <feliperestrepomeneses@gmail.com>
2021-08-22 14:06:10 -0500

    Agregamos el metodo constructor a las clases Account.js y Car.js y modificamos el archivo index.js para llamar el objeto car, con los atributos de Car y Account. Luego en el index.html llamamos esas clases y listo

--- JS/Car.js
+++ JS/Car.js
@@ -1,7 +1,13 @@
-function Car() {
+function Car(license, driver) {
     this.id;
-    this.licence;
-    this.driver;
+    this.licence = license;
+    this.driver = driver;
     this.passenger;
     
+    Car.prototype.printDataCar = function () {
+        console.log(this.driver)
+        console.log(this.driver.name)
+        console.log(this.driver.document)
+    }
+        
 }
