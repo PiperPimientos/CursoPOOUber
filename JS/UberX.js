379efe6bc9b748a84992a160b9e524b519b4f1d9
PiperPimientos <feliperestrepomeneses@gmail.com>
2021-08-23 14:14:42 -0500

    Agregamos las clases UberBlack.js UberPool.js UberVan.js UberX.js y declaramos la clase UberX.js en el index.html para el ejemplo, asi mismo agregamos la clase en el index.html con el new UberX

---
+++ JS/UberX.js
@@ -0,0 +1,7 @@
+class UberX extends Car {
+    constructor(license, driver, brand, model) {
+        super(licennse, driver)
+        this.brand = brand;
+        this.model = model;
+    }
+}
--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,7 @@
 var car = new Car("AW456", new Account("Andres Herrera", "QWE234"));
 car.passenger = 4;
 car.printDataCar();
+
+var uberX = new Car("AW456", new Account("Andrea Ferran", "QWE234"), "Chevrolet", "Spark");
+uberX.passenger = 4;
+uberX.printDataCar();
