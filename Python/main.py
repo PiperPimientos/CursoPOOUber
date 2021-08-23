72f43d5445a3f66bc0db48ba378e54d6a7e28343
PiperPimientos <feliperestrepomeneses@gmail.com>
2021-08-22 19:34:44 -0500

    Agregamos el metodo constructor a python, en los archivos account.py y car.py y modificamos la clase main, importando las clases car y account, para poder utilizarlos en la impresion

--- Python/main.py
+++ Python/main.py
@@ -1,13 +1,9 @@
 from car import Car
+from account import Account
 
 if __name__=="__main__":
     print("Hola Mundo")
-    car = Car()
-    car.license = "AMQ123"
-    car.driver = "Andres Herrera"
-    print(vars(car))
 
-    car2 = Car()
-    car2.license = "QWE576"
-    car2.driver = "Matha"
-    print(vars(car2))
+    car = Car("AMQ123", Account("Andres Herrera", "AND123"))
+    print(vars(car))
+    print(vars(car.driver))
