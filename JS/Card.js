36b9ad070d4624af8a0c86550e5e4733fdb63428
PiperPimientos <feliperestrepomeneses@gmail.com>
2021-08-24 13:45:12 -0500

    Agregamos las clases Card.js Cash.js y Paypal.js que hacen parte de la familia Payment.js

---
+++ JS/Card.js
@@ -0,0 +1,9 @@
+class Card extends Payment(){
+    constructor(id,franquicia,fechaVencimiento,cvv){
+        super(id)
+        this.franquicia = franquicia;
+        this.fechaVencimiento = fechaVencimiento;
+        this.cvv = cvv;
+        
+    }
+}
---
+++ JS/PayPal.js
@@ -0,0 +1,8 @@
+class Ppaypal extends Payment(){
+    constructor(id,referencia,sucursal){
+        super(id)
+        this.referencia = referencia;
+        this.sucursal = sucursal;
+        
+    }
+}
